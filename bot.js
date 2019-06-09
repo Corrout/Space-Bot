9205276be88e4bf7a36600ac587ae9a93eac3e47
SpaceMars8 <51527618+SpaceMars8@users.noreply.github.com>
2019-06-09 18:40:27 +1000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const {prefix, token}= require('./config.json')
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+})
+
+client.login(process.env.BOT_TOKEN)
